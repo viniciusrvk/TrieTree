6a647ca6ddf05d4ff835298066a530f2527fae80
vinicius-rvk <viniciusrvk@gmail.com>
2018-06-14 17:43:46 -0300

    Up the test for this

---
+++ test/arvoretrie/TrieTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arvoretrie;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author viniciusrvk
+ */
+public class TrieTest {
+
+    /**
+     * Test of insertWord method, of class Trie.
+     */
+    @Test
+    public void testInsertWord() {
+        System.out.println("insertWord");
+        String s = "teste";
+        Trie instance = new Trie();
+        instance.insertWord(s);
+        // TODO review the generated test code and remove the default call to fail.
+        assertTrue(instance.search("teste"));
+        
+    }
+    @Test
+    public void testeSearch(){
+        System.out.println("arvoretrie.TrieTest.testeSearch()");
+        String str = "teste";
+        Trie instance = new Trie();
+        instance.insertWord(str);
+        instance.insertWord("teste2");
+        
+        assertTrue(instance.search("teste2"));
+    }
+    
+    @Test
+    public void testRemove() {
+        System.out.println("remove");
+        
+        Trie instance = new Trie();
+        instance.insertWord("teste1");
+        instance.insertWord("teste");
+        boolean expResult = false;
+        boolean result = instance.remove("aaaa");
+        assertTrue(result);
+    }
+    
+    @Test
+    public void testePrimeiro(){
+        Trie instance = new Trie();
+        instance.insertWord("kkk");
+        instance.insertWord("hahaha");
+        TrieNode resultado = instance.getRaiz().primeiro();
+        assertTrue("k".equals(instance.getRaiz().primeiro().getValor()));
+    }
+    
+    @Test
+    public void testeShow(){
+        Trie instance = new Trie();
+        instance.insertWord("teste1");
+        instance.insertWord("teste2");
+        instance.insertWord("aaaa");
+        instance.show();
+        assertTrue(true);
+    }
+    
+}
